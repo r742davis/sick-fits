73ead9ccf1b64a8c1c8a17a676de437fd66fb0f8
Richard Davis <richard.trow.davis@gmail.com>
2020-04-14 12:25:51 -0400

    Added new relation to Data Modal, added permission to createItem mutation

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "90e643578047d6d20ac2";
+/******/ 	var hotCurrentHash = "3b660dda0f0f54e0d296";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
