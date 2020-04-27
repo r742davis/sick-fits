b5b8f3975df71d812e62bcc042e8c97fce56c0f1
Richard Davis <richard.trow.davis@gmail.com>
2020-04-27 11:22:11 -0700

    Initial push before starting next module

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4fdb22894584088ecef0";
+/******/ 	var hotCurrentHash = "eae4016124d7ff0110c6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
