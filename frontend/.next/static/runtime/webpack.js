354762762e07a773c3cd9d51d4f573a3922a3d0e
Richard Davis <richard.trow.davis@gmail.com>
2020-04-26 20:06:44 -0700

    Fixed issue where if item was deleted from database but was still in cart, then an error would be thrown. Refactored CartItem so that if the cartItem.item is not present in the database anymore, a message will be displayed telling the user as such

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "afe9213173867792c0f7";
+/******/ 	var hotCurrentHash = "e2a4d0e4d505626e8178";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
